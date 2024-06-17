44960e447dc96b2441a8fae9d1371bf8b5f76056
Ehsan Eslami <eslami.ehsan200@gmail.com>
2024-06-17 13:55:49 +0330

    fix: fix meta data

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,7 @@ export default function Document() {
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/favicon-32x32.png" sizes="any" />
-        <title>Frontend Mentor | Interactive card details form"</title>
+        <title>Frontend Mentor | Job listings with filtering"</title>
       </Head>
       <body>
         <Main />
