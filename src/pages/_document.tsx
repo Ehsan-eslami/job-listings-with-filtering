5717244baa94bb17500d3e89ef46c309b4e3fde9
Ehsan Eslami <eslami.ehsan200@gmail.com>
2024-06-18 13:48:03 +0330

    Fix: fix meta data

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,8 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta charSet="UTF-8"  name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/favicon-32x32.png" sizes="any" />
         <title>Frontend Mentor | Job listings with filtering"</title>
       </Head>
